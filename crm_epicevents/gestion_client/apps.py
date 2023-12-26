ace05c84c3afdfabd3791bd66b7c183e1db2adeb
Thomas <thomas.savelli92@gmail.com>
2023-12-26 11:55:55 +0100

    mise en place de la structure du project + réalisation des models

---
+++ crm_epicevents/gestion_client/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionClientConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestion_client'
---
+++ crm_epicevents/gestion_client/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Collaborateur(AbstractUser):
+    nom_complet = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    ROLE_CHOICES = [
+        ('commercial', 'Commercial'),
+        ('support', 'Support'),
+        ('gestion', 'Gestion'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, null=False)
+
+    def __str__(self):
+        return self.nom_complet
+
+
+class Client(models.Model):
+    nom_complet = models.CharField(max_length=255, null=False)
+    email = models.EmailField(unique=True, null= False)
+    telephone = models.IntegerField(max_length=12)
+    entreprise = models.CharField(max_length=255)
+    date_creation = models.DateField(null=False)
+    date_derniere_maj = models.DateField(null=False)
+
+    def __str__(self):
+        return self.nom_complet
+
+class Contrat(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, null=False)
+    commercial = models.ForeignKey(Collaborateur, on_delete=models.CASCADE, null=False)
+    montant_total = models.FloatField(null=False)
+    montant_restant = models.FloatField(null=False)
+    date_creation = models.DateField(auto_now_add=True)
+    STATUT_CHOICES = [
+        ('attente signature', 'Attente de signature'),
+        ('signé', 'Signé'),
+        ('en cours', 'En cours'),
+        ('terminé', 'Terminé'),
+        ('résilié', 'Résilié'),
+        ('annulé', 'Annulé'),
+    ]
+    statut = models.CharField(max_length=50, choices=STATUT_CHOICES, null=False)
+
+    def __str__(self):
+        return f"Contrat {self.id} - {self.client}"
+
+
+class Evenement(models.Model):
+    contrat = models.ForeignKey(Contrat, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=255)
+    date_debut = models.DateTimeField()
+    date_fin = models.DateTimeField()
+    support = models.ForeignKey(Collaborateur, on_delete=models.SET_NULL, null=True, blank=True)
+    lieu = models.CharField(max_length=255)
+    nombre_participants = models.IntegerField()
+    notes = models.TextField()
+
+    def __str__(self):
+        return f"Événement {self.id} - {self.nom}"
